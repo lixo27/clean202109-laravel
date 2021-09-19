4f52934159b78f7c683645ee7a925785d65ea3e5
Thiago Senna <trsenna@gmail.com>
2021-09-19 14:40:44 -0300

    adicionado store custommer controller

--- packages/interfaces-api/src/Http/Controllers/Customers/StoreCustomerController.php
+++ packages/interfaces-api/src/Http/Controllers/Customers/StoreCustomerController.php
@@ -2,15 +2,30 @@
 
 namespace Clean\Interfaces\Api\Http\Controllers\Customers;
 
+use Clean\Application\Customers\Commands\StoreCustomer;
+use Clean\Application\Customers\Contracts\StoreCustomerHandlerInterface;
 use Clean\Interfaces\Api\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class StoreCustomerController extends Controller
 {
-    public function __invoke()
+    private StoreCustomerHandlerInterface $storeCustomerHandler;
+
+    public function __construct(StoreCustomerHandlerInterface $storeCustomerHandler)
     {
+        $this->storeCustomerHandler = $storeCustomerHandler;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $command = new StoreCustomer();
+        $command->name = $request->get('name');
+
+        $response = $this->storeCustomerHandler->execute($command);
+
         return response()->json([
             'status' => 'success',
-            'data' => [],
+            'data' => $response,
         ]);
     }
 }
