2cdecabebce9debc583c90fe86bc90516d1b2314
Thiago Senna <trsenna@gmail.com>
2021-09-19 00:17:44 -0300

    added store cutomer controller, but empty

---
+++ Laravel/packages/laravel-interfaces/src/Http/Controllers/Customers/StoreCustomerController.php
@@ -0,0 +1,17 @@
+
+<?php
+
+namespace Clean\Laravel\Interfaces\Http\Controllers\Customers;
+
+use App\Http\Controllers\Controller;
+
+class StoreCustomerController extends Controller
+{
+    public function __invoke()
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => [],
+        ]);
+    }
+}
