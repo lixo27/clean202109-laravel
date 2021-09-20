e0d3760d0aa2c5f6af378da0a8afd1c29b6bf60e
Thiago Senna <trsenna@gmail.com>
2021-09-19 21:24:51 -0300

    created infra-projections project with initial customer finder support

---
+++ packages/infra-projections/src/CustomerFinder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Clean\Infra\Projections;
+
+use Clean\Application\Customers\Contracts\CustomerFinderInterface;
+use Illuminate\Support\Facades\DB;
+
+final class CustomerFinder implements CustomerFinderInterface
+{
+    public function findOne(string $id): array
+    {
+        $customer = DB::table('customers')->find($id);
+
+        return [$customer];
+    }
+
+    public function findAll(): array
+    {
+        $customers = DB::table('customers')->select('id', 'name')->get();
+
+        return $customers->toArray();
+    }
+}
